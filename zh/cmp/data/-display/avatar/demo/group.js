5d86cc4e5a8fe44e5a5d4b3c8f5f8f7750b0728e
xilang <luoxl950809@gmail.com>
2023-02-24 13:09:24 +0800

    Updates

--- zh/cmp/data/-display/avatar/demo/group.js
+++ zh/cmp/data/-display/avatar/demo/group.js
@@ -39,6 +39,19 @@ ReactDOM.render(
       </Tooltip>
       <Avatar style={{ backgroundColor: '#1890ff' }} icon="event_available-o" />
     </Avatar.Group>
+    <Divider />
+    <Avatar.Group maxCount={2} size="small" maxPopoverTrigger="click">
+      <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+      <Avatar style={{ backgroundColor: '#f56a00' }}>K</Avatar>
+      <Tooltip title="User" placement="top">
+        <Avatar style={{ backgroundColor: '#87d068' }} icon="person" />
+      </Tooltip>
+      {new Array(100).fill(0).map((x, index) => (
+        <Avatar key={index} style={{ backgroundColor: '#f56a00' }}>
+          {index + 1}
+        </Avatar>
+      ))}
+    </Avatar.Group>
   </>,
   document.getElementById('container'),
 );
